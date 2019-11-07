2d3e5e193a38137304b700b18b21e537ff9dfc7a
Praveen Akella <44514202+aprave@users.noreply.github.com>
2019-11-07 16:12:41 -0500

    Update ClusterSet.scala

--- similarfiles/src/main/scala/com/edu/cloud/ClusterSet.scala
+++ similarfiles/src/main/scala/com/edu/cloud/ClusterSet.scala
@@ -12,3 +12,26 @@ class ClusterSet(c:ListBuffer[Cluster]){
         return clusters
     }
 }
+    def computeMinhashEstimation() = {
+        var d = new ClusterSet(clusters)
+        MinHash.get_minhash_estimation(d)
+    }
+    /* Calculates closest cluster and returns the distance */
+
+    def getClosestClusters(): (ListBuffer[Cluster], Double) = {
+        val clusterSet = new ClusterSet(clusters)
+        var closest = ListBuffer[Cluster]()
+        var minimumDistance = Double.MaxValue
+        for (c <- clusterSet.getClusters()) {
+            for (d <- clusterSet.getClusters()) {
+                if(c.jaccard.contains(d.clusterId)) {
+                    val dist = c.jaccard(d.clusterId)
+                    if(dist<minimumDistance & c.clusterId!=d.clusterId ){
+                        closest += (c,d)
+                        minimumDistance = dist
+                    }
+                }
+            }
+        }
+     return (closest, minimumDistance)
+    }
