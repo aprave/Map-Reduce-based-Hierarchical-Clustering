9661b7a909596355711b0cbfce912457a1b8a7a2
Praveen Akella <44514202+aprave@users.noreply.github.com>
2019-11-08 11:16:44 -0500

    Update ClusterSet.scala

--- similarfiles/src/main/scala/com/edu/cloud/ClusterSet.scala
+++ similarfiles/src/main/scala/com/edu/cloud/ClusterSet.scala
@@ -3,16 +3,19 @@ package com.edu.cloud
 import scala.collection.mutable.ListBuffer
 
 
-class ClusterSet(c:ListBuffer[Cluster]){
-    var clusters = c
-    def addCluster(c : Cluster): Unit = {
-        clusters+=c
+class ClusterSet(c:ListBuffer[Cluster]) {
+    var clusters: ListBuffer[Cluster] = c
+
+    def addCluster(c: Cluster): Unit = {
+        clusters += c
     }
-    def getClusters() :  ListBuffer[Cluster] ={
+
+    def getClusters(): ListBuffer[Cluster] = {
         return clusters
     }
-}
-    def computeMinhashEstimation() = {
+
+
+    def computeMinhashEstimation(): Unit = {
         var d = new ClusterSet(clusters)
         MinHash.get_minhash_estimation(d)
     }
@@ -34,5 +37,30 @@ class ClusterSet(c:ListBuffer[Cluster]){
             }
         }
      return (closest, minimumDistance)
+    }
+
+    def clusterMinSets(minClusters: (ListBuffer[Cluster], Double)): Unit = {
+        val newKey : (Int) = {(minClusters._1(0).clusterId + minClusters._1(1).clusterId*31) % 10000} //generate new key
+        val fingerPrint: List[Long] = List.concat(minClusters._1(0).fingerPrint, minClusters._1(1).fingerPrint) //merge fingerprints
+        val jac= Map[Int, Double]()
+        val mergedHashes :List[Long] = List.concat(minClusters._1(0).minHash, minClusters._1(1).minHash) //merge minHash
+        var mergedCluster = new Cluster(newKey,fingerPrint ,jac , mergedHashes)
+        var updatedClusterSet = new ListBuffer[Cluster]()
+        for (x<-clusters){
+            if(x.clusterId != minClusters._1(0).clusterId && x.clusterId != minClusters._1(1).clusterId){
+                updatedClusterSet += x
+            }
+        }
+        println(updatedClusterSet , updatedClusterSet.length)
+        for(c <-updatedClusterSet){
+            var newJaccard : Double = MinHash.jaccard_similarity(mergedCluster.minHash.toSet, c.minHash.toSet)
+            mergedCluster.jaccard(c.clusterId)-> newJaccard
+            c.jaccard(mergedCluster.clusterId)-> newJaccard
+            c.jaccard -= minClusters._1(0).clusterId
+            c.jaccard -= minClusters._1(1).clusterId
+            updatedClusterSet += mergedCluster
+            clusters = updatedClusterSet
+
+        }
     }
 }
