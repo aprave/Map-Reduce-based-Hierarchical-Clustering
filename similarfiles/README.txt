3c8a247f202a3e478706c29cf284e7a8dcb5ba6d
Sanket Saurav <snkt087@gmail.com>
2019-11-19 03:12:24 -0500

    Updating for aws emr run

---
+++ similarfiles/README.txt
@@ -0,0 +1,52 @@
+Hadoop MapReduce WordCount Demo
+Example code for CS6240
+Fall 2018
+
+Code author
+-----------
+Joe Sackett
+
+Installation
+------------
+These components are installed:
+- JDK 1.8
+- Scala 2.11.12
+- Hadoop 2.9.1
+- Spark 2.3.1 (without bundled Hadoop)
+- Maven
+- AWS CLI (for EMR execution)
+
+Environment
+-----------
+1) Example ~/.bash_aliases:
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export HADOOP_HOME=/home/joe/tools/hadoop/hadoop-2.9.1
+export SCALA_HOME=/home/joe/tools/scala/scala-2.11.12
+export SPARK_HOME=/home/joe/tools/spark/spark-2.3.1-bin-without-hadoop
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SCALA_HOME/bin:$SPARK_HOME/bin
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
+
+2) Explicitly set JAVA_HOME in $HADOOP_HOME/etc/hadoop/hadoop-env.sh:
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
+Execution
+---------
+All of the build & execution commands are organized in the Makefile.
+1) Unzip project file.
+2) Open command prompt.
+3) Navigate to directory where project files unzipped.
+4) Edit the Makefile to customize the environment at the top.
+	Sufficient for standalone: hadoop.root, jar.name, local.input
+	Other defaults acceptable for running standalone.
+5) Standalone Hadoop:
+	make switch-standalone		-- set standalone Hadoop environment (execute once)
+	make local
+6) Pseudo-Distributed Hadoop: (https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/SingleCluster.html#Pseudo-Distributed_Operation)
+	make switch-pseudo			-- set pseudo-clustered Hadoop environment (execute once)
+	make pseudo					-- first execution
+	make pseudoq				-- later executions since namenode and datanode already running 
+7) AWS EMR Hadoop: (you must configure the emr.* config parameters at top of Makefile)
+	make upload-input-aws		-- only before first execution
+	make aws					-- check for successful execution with web interface (aws.amazon.com)
+	download-output-aws			-- after successful execution & termination
--- similarfiles/src/main/scala/com/edu/cloud/MinHash.scala
+++ similarfiles/src/main/scala/com/edu/cloud/MinHash.scala
@@ -1,6 +1,7 @@
 package com.edu.cloud
 
 import org.apache.log4j.LogManager
+
 import org.apache.log4j.Level
 import org.apache.spark.{SparkConf, SparkContext}
 import java.io.File
@@ -12,6 +13,17 @@ import scala.collection.mutable.ListBuffer
 import scala.concurrent.forkjoin.ThreadLocalRandom
 import scala.util.Random
 
+//With Scala you might now want to use Amazon's official SDK for Java which provides the AmazonS3::listObjects method:
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import com.amazonaws.services.s3.model.ObjectListing
+import org.apache.hadoop.fs.FileSystem
+import java.net.URI
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.LocatedFileStatus
+
+
 object MinHash {
   def main(args: Array[String]) {
     print("Hello")
@@ -22,14 +34,17 @@ object MinHash {
     }
     println(args(0))
     println(args(1))
-    val conf = new SparkConf().setAppName("MinHash").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("MinHash")//.setMaster("local[*]")
     //conf.set("spark.hadoop.validateOutputSpecs", "false");
 
     val sc = new SparkContext(conf)
+    
     //load textfile into spark
     val okFileExtensions = List("txt")
     //create rdd
-    val files = sc.parallelize(getListOfFiles(new File(args(0)), okFileExtensions))
+    //val files = sc.parallelize(getListOfFiles(new File(args(0)), okFileExtensions, sc))
+    val files = sc.parallelize(getListOfFiles( okFileExtensions, sc))
+    println("\n\n\n !----READ COMPLETED----!\n\n\n No of files:"+files.count())
     val random: ThreadLocalRandom = ThreadLocalRandom.current()
     //files.foreach(println)
     var nextPrime = 42949671L
@@ -47,7 +62,8 @@ object MinHash {
     var fileList = files.collect()
     sc.broadcast(ACoeff)
     sc.broadcast(BCoeff)
-    for (filePath <- fileList) {
+    for (filePath <- getListOfFiles( okFileExtensions, sc)) {
+        println("processing file : "+filePath.toString())
         var fingerPrints = sc.textFile(filePath)
             .flatMap(line => line.split(","))
             .map(fp => fp.trim().toLong)
@@ -62,6 +78,7 @@ object MinHash {
         fileSignatureMap +=((filePath.slice(filePath.lastIndexOf("\\")+1,filePath.length())) -> fileSignatures.toList)
     }
     println("completed reading")
+    println("fileSignatureMap size ="+fileSignatureMap.size)
     var convertToList = new ListBuffer[String]()
     for ((fileName,fingerPrints) <- fileSignatureMap) {
       convertToList+=fileName+","+fingerPrints
@@ -128,10 +145,44 @@ object MinHash {
 
 
 
-  def getListOfFiles(dir: File, extensions: List[String]): List[String] = {
-    dir.listFiles.filter(_.isFile).toList.filter { file =>
-      extensions.exists(file.getName.endsWith(_))
-    }.map(_.getPath)
+  //def getListOfFiles(dir: File, extensions: List[String], sc : SparkContext): List[String] = {
+  def getListOfFiles(extensions: List[String], sc : SparkContext): List[String] = {  
+//    dir.listFiles.filter(_.isFile).toList.filter { file =>
+//      extensions.exists(file.getName.endsWith(_))
+//    }.map(_.getPath)
+    val path = "s3://cs6240-mr-bucket/similar-file-input" 
+    val fs = FileSystem.get(URI.create(path), sc.hadoopConfiguration)
+    
+    val it = fs.listFiles(new Path(path), true)
+    val validFiles = mutable.ListBuffer.empty[String]
+    //val validFiles= List[String]()
+    while (it.hasNext()) {
+      val nextFile = it.next();
+      println("next file:"+nextFile.getPath.getName)
+      if(nextFile.isFile() && extensions.exists(nextFile.getPath.getName.endsWith(_))){
+        validFiles+=nextFile.getPath.toString()
+        println("Adding file path: "+nextFile.getPath)
+      }
+    }
+    println("Total files read  : "+validFiles.size)
+    return validFiles.toList
   }
+  
+ //def keys(bucket: String): List[String] = nextBatch(s3Client.listObjects(bucket))
+
+//  def nextBatch(sc:SparkContext, listing: ObjectListing, extensions: List[String]): List[String] = {
+//    val path = "s3://cs6240-mr-bucket/similar-file-input" 
+//    val fs = FileSystem.get(URI.create(path), sc.hadoopConfiguration)
+//    
+//    val it = fs.listFiles(new Path(path), true)
+//    val validFiles= List[String]()
+//    while (it.hasNext()) {
+//      val nextFile = it.next();
+//      if(nextFile.isFile() && extensions.exists(nextFile.getPath.getName.endsWith(_))){
+//        validFiles:+nextFile.getPath
+//      }
+//    }
+//    return validFiles
+//  }
   
 }
